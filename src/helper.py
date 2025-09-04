3e6516a849a12451f547ec85f880cf8fe9fd32ec
Talib <abutalib715@gmail.com>
2025-09-04 12:17:52 +0600

    move jupyter file to python file

---
+++ src/helper.py
@@ -0,0 +1,50 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+# Extract text from PDF
+def load_pdf_files(data):
+    loader =DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    """
+        Given a list Document objects, return a new list of Document objects 
+        containing only 'source','page' in metadata and the original 'page_content'.
+    """
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        page = doc.metadata.get("page")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source": src, "page": page}
+            )
+        )
+
+    return minimal_docs
+
+# Spllit the docs into smaller chunks
+def text_split(minimal_docs): 
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(minimal_docs)
+
+    return text_chunks
+
+# Download the embedding model from HuggingFace
+def download_hugging_face_embeddings():
+    """
+    Download and return the HuggingFace embeddings model.
+    """
+
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(model_name= model_name)
+
+    return embeddings
+
---
+++ src/store_index.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_files, filter_to_minimal_docs, text_split, download_hugging_face_embeddings
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+
+extracted_docs = load_pdf_files(data="data/")
+minimal_docs = filter_to_minimal_docs(extracted_docs)
+text_chunks = text_split(minimal_docs)
+# print(f"Number of chunks: {len(text_chunks)}")
+embedding = download_hugging_face_embeddings()
+
+pinecone_api_key = PINECONE_API_KEY
+pc = Pinecone(pinecone_api_key)
+
+
+index_name = "medical-chatbot"
+
+if not pc.has_index(index_name):
+    pc.create_index(name = index_name,
+                    dimension=384,
+                    metric="cosine",
+                    spec=ServerlessSpec(cloud="aws", region="us-east-1"))
+    
+    index = pc.Index(index_name)
+
+
+# docsearch = PineconeVectorStore.from_documents(
+#     documents=text_chunks,
+#     embedding=embedding,
+#     index_name = index_name
+# )
+
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embedding
+)
